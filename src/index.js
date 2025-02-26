889b3f773b4d3c289dbd2e4568c3c8ebaf56aad1
zaZZa564 <anus_bobra564@mail.ru>
2025-02-26 12:17:40 +0300

    classes w methods

--- src/index.js
+++ src/index.js
@@ -6,19 +6,34 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // components
-const Header = () => {
-  return (
-    <header>header</header>
-  );
+class Header extends React.Component {
+  render() {
+    return(
+      <header>header</header>
+    );
+  }
 }
 
-const Component = () => {
-  return (
-    <div>
-      <Header />
-      <p>some text</p>
-    </div>
-  );
+// class w methods
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <input placeholder='type here'
+          onClick={this.clickLog}
+          onMouseEnter={this.overLog}
+        ></input>
+      </div>
+    );
+  }
+
+  clickLog() {
+    console.log('clicked');
+  }
+  overLog() {
+    console.log('mouse over');
+  }
 }
 
 // render
